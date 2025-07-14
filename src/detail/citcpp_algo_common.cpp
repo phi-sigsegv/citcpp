f142d9285434794536d1d49054ff354e88925995
Philipp Reinkemeier <sigsegv@philipp-r.de>
2025-07-14 11:30:40 +0200

    Also removed recursion step in the number of combination computation.

--- src/detail/citcpp_algo_common.cpp
+++ src/detail/citcpp_algo_common.cpp
@@ -7,23 +7,23 @@ namespace
   // Recursive helper function for combination generation and sum calculation
   // This function will be called by each async task.
   unsigned long long
-  recursive_combine_and_sum (unsigned int start_idx_for_next,
-			     unsigned int current_count,
+  recursive_combine_and_sum (int start_idx_for_next, int current_level,
 			     unsigned long long current_prod_val,
-			     unsigned int t, unsigned int numFactors,
 			     const std::vector<unsigned int> &factorLevels)
   {
-    if (current_count == t)
-      {
-	return current_prod_val;
-      }
-
     unsigned long long partial_sum = 0;
-    for (unsigned int j = start_idx_for_next; j < numFactors; ++j)
+    for (int j = start_idx_for_next; j >= current_level; --j)
       {
-	partial_sum += recursive_combine_and_sum (
-	    j + 1, current_count + 1, current_prod_val * factorLevels[j], t,
-	    numFactors, factorLevels);
+	if (current_level == 0)
+	  {
+	    partial_sum += current_prod_val * factorLevels[j];
+	  }
+	else
+	  {
+	    partial_sum += recursive_combine_and_sum (
+		j - 1, current_level - 1, current_prod_val * factorLevels[j],
+		factorLevels);
+	  }
       }
 
     return partial_sum;
@@ -31,23 +31,26 @@ namespace
 
   unsigned long long
   recursive_combine_and_sum (
-      unsigned int start_idx_for_next, unsigned int current_count,
-      unsigned long long current_prod_val, unsigned int t,
-      unsigned int numFactors, const std::vector<unsigned int> &factorLevels,
+      int start_idx_for_next, int current_level,
+      unsigned long long current_prod_val,
+      const std::vector<unsigned int> &factorLevels,
       const std::vector<unsigned int> &parameter_index_map)
   {
-    if (current_count == t)
-      {
-	return current_prod_val;
-      }
-
     unsigned long long partial_sum = 0;
-    for (unsigned int j = start_idx_for_next; j < numFactors; ++j)
+    for (int j = start_idx_for_next; j >= current_level; --j)
       {
-	partial_sum += recursive_combine_and_sum (
-	    j + 1, current_count + 1,
-	    current_prod_val * factorLevels[parameter_index_map[j]], t,
-	    numFactors, factorLevels, parameter_index_map);
+	if (current_level == 0)
+	  {
+	    partial_sum += current_prod_val
+		* factorLevels[parameter_index_map[j]];
+	  }
+	else
+	  {
+	    partial_sum += recursive_combine_and_sum (
+		j - 1, current_level - 1,
+		current_prod_val * factorLevels[parameter_index_map[j]],
+		factorLevels, parameter_index_map);
+	  }
       }
 
     return partial_sum;
@@ -61,15 +64,19 @@ namespace citcpp
     unsigned long long
     number_of_combinations_to_cover (const model &model, unsigned int t)
     {
-      return recursive_combine_and_sum (0, 0, 1, t,
-					model.get_parameters ().size (),
-					model.get_parameters ());
+      return recursive_combine_and_sum (model.get_parameters ().size () - 1,
+					t - 1, 1, model.get_parameters ());
     }
 
     unsigned long long
     number_of_combinations_to_cover (tf::Executor &executor, const model &model,
 				     unsigned int t)
     {
+      if (t < 2)
+	{
+	  return number_of_combinations_to_cover (model, t);
+	}
+
       const unsigned int numFactors = model.get_parameters ().size ();
 
       // Distribute the initial choices among threads
@@ -78,14 +85,13 @@ namespace citcpp
 
       tf::Taskflow taskflow;
       taskflow.for_each_index (
-	  0u,
+	  t - 1,
 	  numFactors,
-	  1u,
-	  [&model, t, numFactors, &num_combinations]
-	  (unsigned int i)
+	  1,
+	  [&model, t, &num_combinations]
+	  (int i)
 	    {
-	      unsigned long long chunk_num_combos = recursive_combine_and_sum (i+1, 1, model.get_parameters ()[i], t, numFactors,
-		  model.get_parameters ());
+	      unsigned long long chunk_num_combos = recursive_combine_and_sum (i - 1, t - 2, model.get_parameters ()[i], model.get_parameters ());
 	      num_combinations.fetch_add(chunk_num_combos, std::memory_order_acq_rel);
 	    });
 
@@ -99,7 +105,7 @@ namespace citcpp
 	unsigned int current_param_idx, const model &model,
 	const std::vector<unsigned int> &parameter_index_map, unsigned int t)
     {
-      return recursive_combine_and_sum (0, 0, 1, t, current_param_idx,
+      return recursive_combine_and_sum (current_param_idx - 1, t - 1, 1,
 					model.get_parameters (),
 					parameter_index_map);
     }
@@ -110,21 +116,25 @@ namespace citcpp
 	const model &model,
 	const std::vector<unsigned int> &parameter_index_map, unsigned int t)
     {
+      if (t < 2)
+	{
+	  return number_of_combinations_to_cover (current_param_idx, model,
+						  parameter_index_map, t);
+	}
+
       // Distribute the initial choices among threads
       // Each thread will compute a partial sum which we aggregate.
       std::atomic_ullong num_combinations = 0;
 
       tf::Taskflow taskflow;
       taskflow.for_each_index (
-	  0u,
+	  t - 1,
 	  current_param_idx,
-	  1u,
-	  [current_param_idx, &model, &parameter_index_map, t,
-	   &num_combinations]
-	  (unsigned int i)
+	  1,
+	  [&model, &parameter_index_map, t, &num_combinations]
+	  (int i)
 	    {
-	      unsigned long long chunk_num_combos = recursive_combine_and_sum (i+1, 1, model.get_parameters ()[parameter_index_map[i]], t, current_param_idx,
-		  model.get_parameters (), parameter_index_map);
+	      unsigned long long chunk_num_combos = recursive_combine_and_sum (i - 1, t - 2, model.get_parameters ()[parameter_index_map[i]], model.get_parameters (), parameter_index_map);
 	      num_combinations.fetch_add(chunk_num_combos, std::memory_order_acq_rel);
 	    });
 
