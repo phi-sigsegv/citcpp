41489d8e167b96a8bf2849dd5523709cf9367bd2
Philipp Reinkemeier <sigsegv@philipp-r.de>
2025-12-01 13:24:29 +0100

    Use std::reference_wrapper to store a reference to a parameter.   - We need to wrap this in a class however, given that operator== on std::reference_wrapper     is introduced in C++26, which we do not want to depend on.

--- citcpp_lib/include/citcpp/model.hpp
+++ citcpp_lib/include/citcpp/model.hpp
@@ -1,6 +1,7 @@
 #ifndef INPUT_MODEL_HPP_
 #define INPUT_MODEL_HPP_
 
+#include <functional>
 #include <ostream>
 #include <string>
 #include <string_view>
@@ -17,33 +18,35 @@ class parameter_value {
     parameter_value() : value_("") {}
 
     template <typename T>
-    parameter_value(const T &value) : value_(value) {}
+    parameter_value(const T& value) : value_(value) {}
 
-    operator bool() { return std::get<bool>(value_); }
-    operator const std::string &() { return std::get<std::string>(value_); }
-    operator int() { return std::get<int>(value_); }
+    operator bool() const { return std::get<bool>(value_); }
+    operator const std::string&() const {
+      return std::get<std::string>(value_);
+    }
+    operator int() const { return std::get<int>(value_); }
 
     template <typename T>
-    void set_value(const T &value) {
+    void set_value(const T& value) {
       value_ = value;
     }
 
     template <typename T>
-    parameter_value &value(const T &value) {
+    parameter_value& value(const T& value) {
       value_ = value;
       return *this;
     }
 
-    const std::variant<bool, std::string, int> &get_variant_value() const {
+    const std::variant<bool, std::string, int>& get_variant_value() const {
       return value_;
     }
 
-    bool operator==(const parameter_value &other) const {
+    bool operator==(const parameter_value& other) const {
       return value_ == other.value_;
     }
 
-    friend std::ostream &operator<<(std::ostream &os,
-                                    const parameter_value &param_value) {
+    friend std::ostream& operator<<(std::ostream& os,
+                                    const parameter_value& param_value) {
 
       std::visit([&os](auto arg) { os << arg; }, param_value.value_);
 
@@ -61,50 +64,49 @@ enum class parameter_type { BOOLEAN, ENUM, INTEGER };
  */
 class parameter {
   public:
-    const std::string &get_name() const { return name_; }
+    const std::string& get_name() const { return name_; }
 
-    std::string &get_name() { return name_; }
+    std::string& get_name() { return name_; }
 
     void set_name(std::string_view name) { name_ = name; }
 
-    parameter &name(std::string_view name) {
+    parameter& name(std::string_view name) {
       name_ = name;
       return *this;
     }
 
-    const parameter_type &get_type() const { return type_; }
+    const parameter_type& get_type() const { return type_; }
 
-    parameter_type &get_type() { return type_; }
+    parameter_type& get_type() { return type_; }
 
     void set_type(parameter_type type) { type_ = type; }
 
-    parameter &type(parameter_type type) {
+    parameter& type(parameter_type type) {
       type_ = type;
       return *this;
     }
 
-    const std::vector<parameter_value> &get_values() const { return values_; }
+    const std::vector<parameter_value>& get_values() const { return values_; }
 
-    std::vector<parameter_value> &get_values() { return values_; }
+    std::vector<parameter_value>& get_values() { return values_; }
 
-    void set_values(const std::vector<parameter_value> &values) {
+    void set_values(const std::vector<parameter_value>& values) {
       values_ = values;
     }
 
-    parameter &values(const std::vector<parameter_value> &values) {
+    parameter& values(const std::vector<parameter_value>& values) {
       values_ = values;
       return *this;
     }
 
-    void add_value(const parameter_value &value) { values_.push_back(value); }
+    void add_value(const parameter_value& value) { values_.push_back(value); }
 
-    void add_value(parameter_value &&value) {
+    void add_value(parameter_value&& value) {
       values_.push_back(std::move(value));
     }
 
-    bool operator==(const parameter &other) const {
-      return name_ == other.name_ && type_ == other.type_ &&
-             values_ == other.values_;
+    bool operator==(const parameter& other) const {
+      return name_ == other.name_;
     }
 
   private:
@@ -118,23 +120,25 @@ class parameter {
  */
 class parameter_reference {
   public:
-    parameter_reference(const parameter &param) : param_(param) {}
-    parameter_reference(const parameter_reference &other) = default;
-    parameter_reference(parameter_reference &&other) = default;
+    parameter_reference(const parameter& param) : param_(std::cref(param)) {}
+    parameter_reference(const parameter_reference& other) = default;
+    parameter_reference(parameter_reference&& other) = default;
 
     ~parameter_reference() = default;
 
-    parameter_reference &operator=(const parameter_reference &other) = default;
-    parameter_reference &operator=(parameter_reference &&other) = default;
+    parameter_reference& operator=(const parameter_reference& other) = default;
+    parameter_reference& operator=(parameter_reference&& other) = default;
 
-    const parameter &get() const { return param_; }
+    operator const parameter&() const { return get(); }
 
-    bool operator==(const parameter_reference &other) const {
-      return param_ == other.param_;
+    const parameter& get() const { return param_.get(); }
+
+    bool operator==(const parameter_reference& other) const {
+      return get() == other.get();
     }
 
   private:
-    const parameter &param_;
+    std::reference_wrapper<const parameter> param_;
 };
 
 /**
@@ -143,25 +147,25 @@ class parameter_reference {
  */
 class relation {
   public:
-    const std::string &get_name() const { return name_; }
+    const std::string& get_name() const { return name_; }
 
     void set_name(std::string_view name) { name_ = name; }
 
-    const std::vector<parameter_reference> &get_parameters() const {
+    const std::vector<parameter_reference>& get_parameters() const {
       return parameters_;
     }
 
-    std::vector<parameter_reference> &get_parameters() { return parameters_; }
+    std::vector<parameter_reference>& get_parameters() { return parameters_; }
 
-    void add_parameter(const parameter &parameter) {
+    void add_parameter(const parameter& parameter) {
       parameters_.push_back(parameter);
     }
 
-    void add_parameter(const parameter_reference &parameter) {
+    void add_parameter(const parameter_reference& parameter) {
       parameters_.push_back(parameter);
     }
 
-    void add_parameter(parameter_reference &&parameter) {
+    void add_parameter(parameter_reference&& parameter) {
       parameters_.push_back(std::move(parameter));
     }
 
@@ -173,7 +177,7 @@ class relation {
       interaction_strength_ = interaction_strength;
     }
 
-    bool operator==(const relation &other) const {
+    bool operator==(const relation& other) const {
       return name_ == other.name_ && parameters_ == other.parameters_ &&
              interaction_strength_ == other.interaction_strength_;
     }
@@ -189,31 +193,31 @@ class relation {
  */
 class model {
   public:
-    const std::string &get_name() const { return name_; }
+    const std::string& get_name() const { return name_; }
 
     void set_name(std::string_view name) { name_ = name; }
 
-    const std::vector<parameter> &get_parameters() const { return parameters_; }
+    const std::vector<parameter>& get_parameters() const { return parameters_; }
 
-    std::vector<parameter> &get_parameters() { return parameters_; }
+    std::vector<parameter>& get_parameters() { return parameters_; }
 
-    void add_parameter(const parameter &parameter) {
+    void add_parameter(const parameter& parameter) {
       parameters_.push_back(parameter);
     }
 
-    void add_parameter(parameter &&parameter) {
+    void add_parameter(parameter&& parameter) {
       parameters_.push_back(std::move(parameter));
     }
 
-    const std::vector<relation> &get_relations() const { return relations_; }
+    const std::vector<relation>& get_relations() const { return relations_; }
 
-    std::vector<relation> &get_relations() { return relations_; }
+    std::vector<relation>& get_relations() { return relations_; }
 
-    void add_relation(const relation &r) { relations_.push_back(r); }
+    void add_relation(const relation& r) { relations_.push_back(r); }
 
-    void add_relation(relation &&r) { relations_.push_back(std::move(r)); }
+    void add_relation(relation&& r) { relations_.push_back(std::move(r)); }
 
-    bool operator==(const model &other) const {
+    bool operator==(const model& other) const {
       return name_ == other.name_ && parameters_ == other.parameters_ &&
              relations_ == other.relations_;
     }
