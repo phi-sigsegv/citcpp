f889586deaedd7d33c90cc329834d5bf9bb82dd8
Philipp Reinkemeier <sigsegv@philipp-r.de>
2026-07-19 22:32:07 +0200

    Removed sylvan source code.

--- 3rd_party/sylvan/test/install/consumer-cmake/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p build
-cmake -S . -B build -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}"
-cmake --build build
-./build/test
-
--- 3rd_party/sylvan/test/install/consumer-pkgconfig/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "sylvan.h"
-
-int main() {
-    return 0;
-}
-
